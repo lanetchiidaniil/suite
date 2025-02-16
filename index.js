ec1d65a64fea66a142c0fe63e6042b0cac04156b
Lanetchi Daniel <lanetchiidaniil@gmail.com>
2025-02-16 10:26:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.querySelector('.burger-menu').addEventListener('click', function () {
     document.querySelector('nav').classList.toggle('active');
 });
+
+document.querySelector('.hidden-menu').addEventListener('click', () => {
+    document.querySelector('.hidden-menu').classList.add('active');
+})
